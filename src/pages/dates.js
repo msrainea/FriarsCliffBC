6bc90f1011bcd9b7237617fdad2a0c4a92ef4c0c
msrainea <amelia.raine@virginmedia.com>
2024-11-09 09:36:56 +0000

    Updated dates

--- src/pages/dates.js
+++ src/pages/dates.js
@@ -43,9 +43,16 @@ class Dates extends React.Component {
                                 <td>12.30pm</td>
                                 <td><a href="https://fb.me/e/44LhWD0vQ" target="_blank" rel="noopener noreferrer">Facebook event page</a></td>
                             </tr>
+                            <tr className="past">
+                                <td>CANCELLED Sunday</td>
+                                <td>3rd November 2024</td>
+                                <td>10.30am</td>
+                                <td>12.30pm</td>
+                                <td><a href="https://fb.me/e/3nQtKBs3X" target="_blank" rel="noopener noreferrer">Facebook event page</a></td>
+                            </tr>
                             <tr className="next">
                                 <td>Sunday</td>
-                                <td>3rd November 2024</td>
+                                <td>17th November 2024</td>
                                 <td>10.30am</td>
                                 <td>12.30pm</td>
                                 <td><a href="https://fb.me/e/3nQtKBs3X" target="_blank" rel="noopener noreferrer">Facebook event page</a></td>
@@ -74,8 +81,12 @@ class Dates extends React.Component {
                             Sunday 18th August 2024, 10.30am - 12.30pm<br/>
                             <a href="https://fb.me/e/44LhWD0vQ" target="_blank" rel="noopener noreferrer">Facebook event page</a>
                         </div>
+                        <div className="datesLine past">
+                            CANCELLED Sunday 3rd November 2024, 10.30am - 12.30pm<br/>
+                            <a href="https://fb.me/e/3nQtKBs3X" target="_blank" rel="noopener noreferrer">Facebook event page</a>
+                        </div>
                         <div className="datesLine next">
-                            Sunday 3rd November 2024, 10.30am - 12.30pm<br/>
+                            Sunday 17th November 2024, 10.30am - 12.30pm<br/>
                             <a href="https://fb.me/e/3nQtKBs3X" target="_blank" rel="noopener noreferrer">Facebook event page</a>
                         </div>
                         <div className="datesLine">
