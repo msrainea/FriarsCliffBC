750699435fdc456d909c95dd7b3ed507195e71e0
msrainea <amelia.raine@virginmedia.com>
2025-01-12 15:57:06 +0000

    Updated dates

--- src/pages/dates.js
+++ src/pages/dates.js
@@ -6,7 +6,7 @@ class Dates extends React.Component {
             <div className = "pageContent">
                 <div className="textContent">
                     <h2>Event Dates</h2>
-                    <p>The following dates have been scheduled for 2024.</p>
+                    <p>The following dates have been scheduled for 2025.</p>
                     <p>Please be aware that all dates and times are subject to change. Please check before attending an event. Updates will be sent via email to the mailing list and posted on social media.</p>
                     <br/>
                 </div>
